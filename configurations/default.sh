ffc649ba4281e03c2ef6cb37f9e6a9c1495472c4
Xiaoxing Cheng <hello@chengxiaoxing.me>
2023-07-26 07:38:46 +0800

    initial commit

--- configurations/default.sh
+++ configurations/default.sh
@@ -8,6 +8,7 @@ root="$script_dir/.."
 source_dir="$script_dir/../source"
 build_dir="$script_dir/../build/$config"
 install_dir="$script_dir/../install/$config"
+qt_dir="$HOME/ocp/external/qt/6.3.0/install"
 
-cmake -S $source_dir -B $build_dir -DCMAKE_INSTALL_PREFIX=$install_dir -G Ninja
+cmake -S $source_dir -B $build_dir -DCMAKE_INSTALL_PREFIX=$install_dir -G Ninja -DCMAKE_PREFIX_PATH=$qt_dir
 cmake --build $build_dir --target install
--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -2,27 +2,35 @@
 # You should modify this script to prepare the folder structure for external project source codes
 # The source code must be in ocp/external/external_id/version/source folder
 # get command line arguments
-version=${1}
-external_id=${2}
+version=${1:-11.0.0}
+external_id=${2:-qtcreator}
 
 # prepare necessary variables
-url="https://github.com/gabime/spdlog/archive/refs/tags/v$version.tar.gz"
 dir="ocp/external/$external_id/$version/source"
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 gz_file="$external_id-v$version.tar.gz"
+pwd=$(pwd)
 # move to the root of the external repository
 cd $script_dir/..
 
 # start the script
 rm -rf *.tar.gz
-wget -O $gz_file $url
-echo "Download $external_id $version"
+
 rm -rf ocp
 echo "Remove previous ocp folder"
+
 mkdir -p "$dir"
 echo "Create ocp folder structure"
-tar -xzf $gz_file -C $dir --strip-components=1
-echo "Extract files to ocp folder"
+cd $dir/..
+
+git clone https://github.com/qt-creator/qt-creator.git source
+cd source
+git submodule update --init --recursive
+
+echo "Download $external_id $version"
+
+
+cd $script_dir/..
 cp -r configurations "$dir/.."
 echo "Copy all configurations to ocp folder"
 cp -r cmake "$dir/.."
@@ -31,4 +39,4 @@ cp ocp.yml "$dir/.."
 echo "Copy ocp.yml to ocp folder"
 
 # move back to the folder where you started
-cd -
+cd $pwd
