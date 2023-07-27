e03d59c5f4efd2efb6e1e198e8acdd9912e0a2d1
Xiaoxing Cheng <hello@chengxiaoxing.me>
2023-07-27 10:18:52 +0800

    update dependency to qt 6.5.2

--- configurations/default.sh
+++ configurations/default.sh
@@ -8,7 +8,7 @@ root="$script_dir/.."
 source_dir="$script_dir/../source"
 build_dir="$script_dir/../build/$config"
 install_dir="$script_dir/../install/$config"
-qt_dir="$HOME/ocp/external/qt/6.3.0/install"
+qt_dir="$HOME/ocp/external/qt/6.5.2/install/default"
 
 cmake -S $source_dir -B $build_dir -DCMAKE_INSTALL_PREFIX=$install_dir -G Ninja -DCMAKE_PREFIX_PATH=$qt_dir
 cmake --build $build_dir --target install
